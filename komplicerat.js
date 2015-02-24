807b1683424960c08643def799b992bd6c6fd3a7
viktja710 <viktja710@E29620.edu.linkoping.se>
2015-02-24 13:59:37 +0100

    loop no workerino, filter också

--- komplicerat.js
+++ komplicerat.js
@@ -3,7 +3,15 @@ $(function(){
                 var JSONtesting = [];
                 $.getJSON('JSONtesting.json', function(JSONtesting){
                     console.log(JSONtesting.lesmonkeys);
-                    $('body').append(JSONtesting.lesmonkeys[0].Apjefvel);
+//                    $('body').append(JSONtesting.lesmonkeys[0].Apjefvel);
+//                    $('body').append(JSONtesting.lesmonkeys[0].lastName);
+//                    $('body').append(JSONtesting.lesmonkeys[1].Apjefvel);
+//                    $('body').append(JSONtesting.lesmonkeys[1].lastName);
+//                    $('body').append(JSONtesting.lesmonkeys[2].Apjefvel);
+//                    $('body').append(JSONtesting.lesmonkeys[2].lastName);
+                    $('body').each(function(JSONtesting){
+                        $('body').append(JSONtesting.lesmonkeys.Apjefvel);
+                    });
                 });
             });
             
